ae0fb13455001fcc953718ba1eeadfa6d04e252d
Adelson Developer <desouzaadelson@hotmail.com>
2024-05-10 12:32:07 -0300

    Staring the first codes with swift language

---
+++ curso_swift_lang/declaracaoImplicita.swift
@@ -0,0 +1,16 @@
+/**
+ * Declaração Implicita
+  Na declaração implícita, nós não precisamos especificar o tipo de dado que a variável ou constante será.
+  Neste caoso, o compilador irá inferir o tipo de dado da variável ou constante com base no valor que ela recebe.
+*/
+
+var declaracaoImplicita = "Isso é uma declaração implícita"
+print(declaracaoImplicita)
+
+// Other Example
+// Com base no valor passado para a variável, o tipo de dada é inferido para Int.
+var number = 10 
+print(number)
+
+var number1 = 12.50 // Com base no valor passado para a variável, o tipo de dada é inferido para Double.
+print(number1)
